620c3284b551cd5c97b85322f5644ce81ccbbfa3
Jann Fraulien Madduma <madduma.jannfraulien@gmail.com>
2022-11-05 11:05:22 +0800

    auth

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const { getGoals, setGoal, updateGoal, deleteGoal } = require('../contollers/goalController');
+const express = require('express')
+const { getGoals, setGoal, deleteGoal, updateGoal } = require('../contollers/goalController')
 const router = express.Router()
 
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoal)
+const { protect } = require('../middleware/authMiddleware')
 
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
 
 module.exports = router
